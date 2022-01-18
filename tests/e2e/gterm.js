52d6270469f73546391781f0dc56cd179f46ab1f
Naveen K R <thenaveenkr7@gmail.com>
2022-01-18 13:41:09 +0530

    update e2e test - you can now add command and expected output to tests/e2e/output.cjs

--- tests/e2e/gterm.js
+++ tests/e2e/gterm.js
@@ -1,42 +1,14 @@
-const { exec, spawn } = require('child_process');
-const colors = require('./output.cjs');
+const { exec } = require('child_process');
+const { expectations: exp } = require('./output.cjs');
 
 describe('gterm', () => {
-    it('should return \'rainbow colors\' in rainbow colors', done => {
-        const test = exec('echo \'rainbow colors\' | node . violet indigo blue green yellow orange red');
-        const expected = colors.rainbow;
-        test.stdout.on('data', data => {
-            expect(data).toBe(expected);
-            done()
-        });
-    });
-    it('should return \'red\' in red color', done => {
-        const test = exec('echo "red" | node . red');
-        const expected = colors.red;
-        test.stdout.on('data', data => {
-            expect(data).toBe(expected);
-            done()
-        });
-    });
-    it('should return \' cols > str\' in given color', done => {
-        const test = exec('echo \'cols > str\' | node . red orange blue cyan green lightblue pink black white gray yellow orange');
-        const expected = colors.extraCols;
-        test.stdout.on('data', data => {
-            expect(data).toBe(expected);
-            done()
-        });
-    });
-    it('should return \'testing \\n multiline\' in pink and lightblue color', done => {
-        const test = spawn('echo \'testing \\n multiline\' | node . pink lightblue', {
-            shell: true
-        });
-        const expected = colors.multiline;
+    it.each(exp)('should return $out when \'$cmd\' given', ({cmd, out}, done) => {
+        const test = exec(cmd);
         const output = [];
-        test.stdout.on('data', data => {
-            output.push(data)
-        });
+        test.stdout.on('data', (chunk) => output.push(chunk));
         test.stdout.on('end', () => {
-            expect(Buffer.concat(output).toString()).toBe(expected);
+            const result = output.join('');
+            expect(result).toBe(out);
             done()
         })
     });
