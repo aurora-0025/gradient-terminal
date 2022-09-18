61b0b9cf12496b24e685d06e3606d241d8e4e9b5
Ritin George <ritingeorge.d@gmail.com>
2022-09-18 18:43:55 +0530

    Version 1.2.0

--- bin/gradient.js
+++ bin/gradient.js
@@ -1,54 +1,89 @@
+/**
+ * It takes a list of colors, a string, and a max length, and returns a string with the colors applied
+ * to the string
+ * @param colors - An array of colors.
+ * @param data - The string you want to apply the gradient to
+ * @param max_length - The maximum length of the string you want to apply the gradient to.
+ * @returns A string with ANSI escape codes.
+ */
+
 import tinygradient from "tinygradient";
 import tinycolor from "tinycolor2";
+import ansiRegex from "ansi-regex";
 
-export function applyGradient(colors, data, max_length) {
-  let invalidColors = [];
-  for (var color of colors) {
-    if (!tinycolor(color).isValid()) {
-      if (!invalidColors.includes(color)) {
-        invalidColors.push(color);
-      }
+export const applyGradient = (colors, data, max_length) => {
+    let out = [];
+    let rgb = [];
+ 
+    /* Returning an empty string if the max_length is 0. */
+    if (max_length == 0) return "";
+
+    /* Checking if the length of the colors array is greater than the max_length. If it is, it is slicing
+    the colors array to the max_length. */
+    if (colors.length > max_length) {
+        colors = colors.slice(0, max_length);
     }
-  }
-  if (invalidColors.length != 0) {
-    if (invalidColors.length == 1) return process.stdout.write(`${invalidColors} is not an accepted color\n`)
-    return process.stdout.write(`${invalidColors} are not accepted colors\n`);
-  }
 
-  if (max_length == 1) {
-    let rgb = tinycolor(colors[0]);
-    let r = Math.round(rgb._r);
-    let g = Math.round(rgb._g);
-    let b = Math.round(rgb._b);
-
-    return process.stdout.write(`\x1b[38;2;${r};${g};${b}m${data}\x1b[0m`);
-  }
-
-  if (colors.length > max_length) {
-    colors = colors.slice(0, max_length);
-  }
-
-  var gradient = tinygradient(colors);
-  var hsv = gradient.hsv(max_length, "short");
-  let rgb = hsv.map((color) => [color._r, color._g, color._b]);
-  let out = [];
-
-  var text = data.split("");
-  var val = 0;
-  for (var ele of text) {
-    if (ele != " ") {
-      let r = Math.round(rgb[val][0]);
-      let g = Math.round(rgb[val][1]);
-      let b = Math.round(rgb[val][2]);
-      out.push(`\x1b[38;2;${r};${g};${b}m${ele}\x1b[0m`);
-      val += 1;
-    } else {
-      out.push(" ");
-      val += 1;
+    /* Checking if the max_length is 1. If it is, it is setting the rgb array to the rgb values of the
+    first color. If it isn't, it is creating a gradient from the colors, and then it is mapping the rgb
+    values of the gradient to the rgb array. */
+    if (max_length == 1) rgb[0] = [tinycolor(colors[0])._r, tinycolor(colors[0])._g, tinycolor(colors[0])._b];
+    else {
+      let gradient = tinygradient(colors);
+      let hsv = gradient.hsv(max_length, "short");
+      rgb = hsv.map((color) => [color._r, color._g, color._b]);
     }
-  }
-  out = out.join("");
-
-  return process.stdout.write(out + '\n');
+    let withoutAnsi = data.replace(ansiRegex(), "%^placeholder^%");
+    let matchedAnsi = data.match(ansiRegex());
+    if (!matchedAnsi) matchedAnsi = [];
+    withoutAnsi = withoutAnsi.split("%^placeholder^%");
+    let line = [];
+    let val = 0;
+    for (let i = 0; i < withoutAnsi.length; i++) {
+        if (matchedAnsi[i - 1]) {
+            let match = matchedAnsi[i - 1].match(
+                new RegExp(/(\d+;+(?!(\dH)))|(\d+m)/g)
+            );
+            let cursor = matchedAnsi[i - 1].match(
+                new RegExp(/(\d+$)|[A-Z]+$/g)
+            );
+            if (cursor) {
+                line.push(matchedAnsi[i - 1]);
+            }
+            if (match) {
+                for (let i = 0; i < match.length; i++) {
+                    match[i] = match[i].replace(new RegExp(/(?=\d)*m/g), ";");
+                }
+                withoutAnsi[i].split("").forEach((char) => {
+                    let r = Math.round(rgb[val][0]);
+                    let g = Math.round(rgb[val][1]);
+                    let b = Math.round(rgb[val][2]);
+                    out.push(
+                        `\x1b[${match.join(
+                            ""
+                        )}38;2;${r};${g};${b}m${char}\x1b[0m`
+                    );
+                    val += 1;
+                });
+            } else {
+                withoutAnsi[i].split("").forEach((char) => {
+                    let r = Math.round(rgb[val][0]);
+                    let g = Math.round(rgb[val][1]);
+                    let b = Math.round(rgb[val][2]);
+                    out.push(`\x1b[${""}38;2;${r};${g};${b}m${char}\x1b[0m`);
+                    val += 1;
+                });
+            }
+        } else {
+            withoutAnsi[i].split("").forEach((char) => {
+                let r = Math.round(rgb[val][0]);
+                let g = Math.round(rgb[val][1]);
+                let b = Math.round(rgb[val][2]);
+                out.push(`\x1b[${""}38;2;${r};${g};${b}m${char}\x1b[0m`);
+                val += 1;
+            });
+        }
+    }
+    out = out.join("");
+    return out;
 }
-
--- bin/index.js
+++ bin/index.js
@@ -1,35 +1,72 @@
 #!/usr/bin/env node
 
-import { applyGradient } from './gradient.js';
+import ansiRegex from "ansi-regex";
+import tinycolor from "tinycolor2";
+import { applyGradient } from "./gradient.js";
 
-var args;
-var gradientColors;
-var lines;
-var linechars;
+let args;
+let gradientColors;
 
-process.stdin.setEncoding('utf8');
-process.stdin.on("data", function (data) {
-  main((data));
-});
+if (process.stdin.isTTY) {
+    console.log(`\x1b[1;31;4mMissing Pipe Command\x1b[0m`)
+    console.log(`\x1b[1musage: \x1b[1;31;4;1m<command>\x1b[24;0m | gterm <color1 color2 ...>\x1b[0m`);
+} else {
+    process.stdin.resume();
+    // Gather all data
+    process.stdin.on("data", function (chunk) {
+        let data = chunk.toString("utf8");
+        main(data);
+    });
 
-function main(data) {
-  [, , ...args] = process.argv;
-  gradientColors = args;
+    // Save when finished
+    process.stdin.on("end", function () {
+        /* It resets the color of the terminal. */
+        process.stdout.write("\x1b[0m");
+    });
+}
 
-  if (gradientColors.length == 0) {
-    return process.stdout.write(`usage <cmd> | gterm <color1 color2 ...>\n`);
-  }
+const main = (data) => {
+    let invalidColors = [];
+    /* Getting the arguments */
+    [, , ...args] = process.argv;
+    gradientColors = args;
+    /* Checking if the user has provided any colors. If not, it will print the usage. */
+    if (!gradientColors[0]) {
+        console.log(`\x1b[1;31;4mMissing Colors\x1b[0m`)
+        console.log(`\x1b[1musage: <command> | gterm \x1b[1;31;4;1m<color1 color2 ...>\x1b[0m`);
+        return;
+    }
 
-  if (gradientColors.length == 1) {
-    gradientColors.push(gradientColors[0])
-  }
+    /* If the user only provides one color, it will duplicate it so that the gradient can be applied. */
+    if (gradientColors.length == 1) {
+        gradientColors.push(gradientColors[0]);
+    }
 
-  lines = data.split("\n");
-  linechars = [];
-  let max_length = Math.max(...lines.map((el) => el.length));
-  for (var line of lines) {
-    if (line != "") {
-      applyGradient(gradientColors, line, max_length);
-    } else process.stdout.write(line) + '\n';
-  }
+    /* Checking if the colors are valid. */
+    for (const color of gradientColors) {
+        if (!tinycolor(color).isValid()) {
+            invalidColors.push(color);
+        }
+    }
+    if (invalidColors.length != 0) {
+        console.log(`\x1b[1;31;4mInvalid Colors\x1b[0m`)
+        console.log(`\x1b[1m${invalidColors.join(" ")}\x1b[0m`);
+        return;
+    }
+
+    let lines = data.split("\n");
+    /* Getting the length of the longest line. */
+    let max_length = Math.max(
+        ...lines.map((el) => {
+            el = el.replace(ansiRegex(), "");
+            return el.length;
+        })
+    );
+
+    for (let i = 0; i < lines.length; i++) {
+        /* Applying the gradient to the line. */
+        let line = applyGradient(gradientColors, lines[i], max_length);
+        process.stdout.write(line);
+        if (i != lines.length - 1) process.stdout.write("\n");
+    }
 }
